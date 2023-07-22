f4aea61870c79fa8258bc59f2f7b4473ab3bca1c
Kristian Emmanuel Padilla <ketpadilla@mymail.mapua.edu.ph>
2023-07-22 05:28:50 +0000

    add setup

---
+++ app.py
@@ -0,0 +1,10 @@
+# IMPORTED MODULES
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request, render_template_string
+
+# CONFIGURE APPLICATION
+app = Flask(__name__)
+
+# RUN FLASK APPLICATION
+if __name__ == "__main__":
+    app.run()
